26d449356b056758efac86ab150b006f684a7e8b
5TAHUR5 <salihovtahir2007@gmail.com>
2022-06-24 21:35:46 +0200

    fgh2

--- main.js
+++ main.js
@@ -20,6 +20,43 @@ window.addEventListener("mousedown", (e) => draw = true)
 // Set draw to false when mouse is released
 window.addEventListener("mouseup", (e) => draw = false)
 
+window.addEventListener("touchstart", (e) => draw = true)
+
+window.addEventListener("touchend", (e) => draw = false)
+
+window.addEventListener("touchmove", (e) => {
+
+    const distX = e.clientX - (canvas.width / 2)
+    const distY = e.clientY - (canvas.height / 2)
+
+    if (distX > 0) {
+
+        penis.style.transform = `rotate(${Math.acos( distY / Math.sqrt(distX*distX + distY*distY))}rad)`
+    }
+    if (distX < 0) {
+        penis.style.transform = `rotate(-${Math.acos( distY / Math.sqrt(distX*distX + distY*distY))}rad)`
+    }
+
+    // if draw is false then we won't draw
+    if(prevX == null || prevY == null || !draw){
+        prevX = e.clientX
+        prevY = e.clientY
+        return
+    }
+
+    let currentX = e.clientX
+    let currentY = e.clientY
+
+
+    ctx.beginPath()
+    ctx.moveTo(prevX, prevY)
+    ctx.lineTo(currentX, currentY)
+    ctx.stroke()
+
+    prevX = currentX
+    prevY = currentY
+})
+
 window.addEventListener("mousemove", (e) => {
 
     const distX = e.clientX - (canvas.width / 2)
